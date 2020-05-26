975922a2acbfded118e6f7fcae538817a757784b
AT <734237292@qq.com>
2020-05-27 06:37:58 +0800

    feat(reducers): delete deadcode

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
+import user from './user'
+import post from './post'
+import users from './users'
 
 export default combineReducers({
-  counter
+  user,
+  post,
+  users,
 })
